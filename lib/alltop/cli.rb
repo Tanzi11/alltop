60a61a32bac6bf46bc9c799f8a18a75da0fd26ea
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-11-23 02:57:37 -0500

    create CLI method to display news outlets

--- lib/alltop/cli.rb
+++ lib/alltop/cli.rb
@@ -2,18 +2,27 @@ class CLI
 
 def call
   welcome
-  # title_list
+  puts "************************************************************"
+  title_list
+  puts "************************************************************"
   # article_list
   # done
 end
 
 def welcome
-  puts "Welcome informed citizen! Where do you want today's news from?"
+  puts "Welcome informed citizen! What are you excited to read?"
 end
 
-# def title_list
-#   puts Scraper.scrape
-# end
+def title_list
+  array = Scraper.scrape
+  array_new = array.values_at(0,2,4,6,8,10,12,14,16,18,20,22,24,26)
+  array_new.each_with_index do |i, array_new|
+  puts " #{array_new}. #{i}"
+end
+end
 
 
 end
+
+# array_new = array.partition.each_with_index{ |el, i| i.even? }
+# list = array_new[0].delete_if {|i| i == "About" || i == "Privacy Policy" || i == "AllTop.com"}
--- lib/alltop/scraper.rb
+++ lib/alltop/scraper.rb
@@ -1,28 +1,29 @@
 class Scraper
-  # attr_accessor :title
-  #
-  # @@all = []
-  #
-  # def initialize(title=nil)
-  #   @title = title
-  #   @@all << self
-  # end
-  #
-  # def self.all
-  #   @@all
-  # end
+  attr_accessor :title
+
+  @@all = []
+
+  def initialize(title=nil)
+    @title = title
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 
 
 def self.scrape
   doc = Nokogiri::HTML(open("https://alltop.com/"))
   publication_names = doc.css("div.container p")
-   publication_names.css("a").collect do |name|
-   title = name.children.text
+  publication_names.css("a").collect do |name|
+    title = name.children.text
 end
 end
 
-# each do |name|
-# title = name.children.text
+
+# publication_names.css("a").children.collect do |name|
+# title = name.text
 
 # publication_names = doc.css("div.container p")
 # name = publication_names.css("a").children.text
