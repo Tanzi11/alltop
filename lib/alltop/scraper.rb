8d0f3b91ecbc15c3794257ee5676b099546ec84a
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-11-25 22:41:25 -0500

    scraping methods for Article complete

--- lib/alltop/scraper.rb
+++ lib/alltop/scraper.rb
@@ -2,85 +2,40 @@ class Scraper
 
 def self.scrape
   doc = Nokogiri::HTML(open("https://alltop.com/"))
-  publication_names = doc.css("div.col-xs-12.col-md-4 p")
-  publication_names.css("a")[0..-2].each do |node|
-    title = node.text.split("(")[0]
-    url = node.attribute("href").value
-    AllTop.new(title,url) unless title.empty?
+  publication_names = doc.css("div.col-xs-12.col-md-4")
+  publication_names.each do |node|
+    node.css("p a")[0..-2].each do |ele|
+      title = ele.text.split("(")[0]
+      url = ele.attribute("href").value
+      AllTop.new(title,url) unless title.empty?
+  end
+     alltop_obj = AllTop.all.last
+     node.css("ul").each do |li|
+       title = li.css("a").children[0].text
+       url = li.css("a.one-line-ellipsis").attribute("href").value
+       quote = li.css("a.one-line-ellipsis").attribute("data-content").value.split("<br>")[1]
+       Article.new(title,url,quote,alltop_obj)
+     end
 end
 end
 
-def self.scrape_quotes
-  doc = Nokogiri::HTML(open("https://alltop.com/"))
-  quotes = doc.css("div.container")
-  quotes.css("a.one-line-ellipsis").map do |quote|
-    list = quote.attribute("data-content").value.gsub(/<\/?[^>]*>/, "")
-end
-end
 
-def self.scrape_trending_items
-  doc = Nokogiri::HTML(open("https://alltop.com/"))
-  items = doc.css("div.container li")
-  items.css("a").collect do |item|
-    line = item.children.text
-end
-end
-
-def self.techcrunch_articles
-Scraper.scrape_trending_items[46..53]
-end
-
-def self.wired_articles
-Scraper.scrape_trending_items[54..61]
-end
-
-def self.nyt_articles
-Scraper.scrape_trending_items[62..69]
-end
-
-def self.mashable_articles
-Scraper.scrape_trending_items[70..77]
-end
-
-def self.wp_articles
-Scraper.scrape_trending_items[78..85]
-end
-
-def self.npr_articles
-Scraper.scrape_trending_items[86..93]
-end
-
-def self.reddit_articles
-Scraper.scrape_trending_items[94..101]
-end
-
-def self.verge_articles
-Scraper.scrape_trending_items[102..109]
-end
-
-def self.techmeme_articles
-Scraper.scrape_trending_items[110..117]
-end
-
-def self.viral_articles
-Scraper.scrape_trending_items[118..125]
-end
-
-def self.cnn_articles
-Scraper.scrape_trending_items[126..133]
-end
-
-def self.bbc_articles
-Scraper.scrape_trending_items[134..141]
-end
-
-def self.reuters_articles
-Scraper.scrape_trending_items[142..149]
-end
-
-def self.recode_articles
-Scraper.scrape_trending_items[150..157]
-end
+# def self.scrape_quotes
+#   doc = Nokogiri::HTML(open("https://alltop.com/"))
+#   quotes = doc.css("div.container")
+#   quotes.css("a.one-line-ellipsis").map do |quote|
+#     list = quote.attribute("data-content").value.gsub(/<\/?[^>]*>/, "")
+# end
+# end
+#
+# def self.scrape_trending_items
+#   doc = Nokogiri::HTML(open("https://alltop.com/"))
+#   items = doc.css("div.container li")
+#   items.css("a").collect do |item|
+#     line = item.children.text
+# end
+# end
+#
 
 
 
