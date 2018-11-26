8ccdc6be268b5dbaadf29cf440747212fc180572
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-11-25 19:00:58 -0500

    add article class and initialize method

---
+++ lib/alltop/article.rb
@@ -0,0 +1,13 @@
+class Article
+  attr_accessor :title, :article_url, :quote, :alltop
+
+  def initialize(title,url,quote,alltop)
+    @title = title
+    @article_url = article_url
+    @quote = quote
+    @alltop = alltop
+  end
+
+
+
+end
