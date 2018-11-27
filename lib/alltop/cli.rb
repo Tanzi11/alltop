e734cdb768f4384932e68560824f9e79fd542eb5
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-11-26 19:18:24 -0500

    remove old code used for testing

--- lib/alltop/cli.rb
+++ lib/alltop/cli.rb
@@ -27,55 +27,22 @@ end
 def get_user_input
   puts "Select the number:"
   input = gets.strip.to_i
+
   @selected_index = input - 1
 end
 
 def display_articles
+alltop_obj = AllTop.all[@selected_index]
+alltop_obj.articles.each do |article_obj|
   binding.pry
-Scraper.scrape
-AllTop.all.each do |alltop_obj, i|
-  puts "#{alltop_obj.articles.first}"
+  puts "Article Title: #{article_obj.title}\n\n"
+  puts
+  puts "For link: #{article_obj.article_url}"
+  puts "For more info: "
 end
 end
 
 
-
-# def menu
-#   input = nil
-#     until input == "done"
-#       puts ""
-#       puts "Where do you want to go?"
-#       input = gets.chomp.to_i
-#
-#       if input.to_i == "1"
-#         puts "TechCrunch"
-#         puts "#{article_obj}"
-# end
-# end
-
-
-
-
-# def fix_trending_items
-#   array = Scraper.scrape_trending_items
-# end
-#
-# def article_list
-#   array_new = fix_trending_items.values_at(46..50)
-#   array_new.each_with_index do |array_new, i|
-#        puts "#{i+1}. #{array_new}"
-#      end
-#    end
-
-
-
-# def article_list
-#   array = Scraper.scrape_trending_items
-#   array_new = array.values_at(46..-1)
-#   array_new.each_with_index do |array_new, i|
-#     puts "#{i+1}. #{array_new}"
-# end
-# end
 
 
 def done
